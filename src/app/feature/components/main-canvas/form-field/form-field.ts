cc73b457e65599881f7eacc9345d5b326f9864e1
BORITH-Monorom <borithmonorom@gmail.com>
2025-08-18 01:41:32 +0700

    update

--- src/app/feature/components/main-canvas/form-field/form-field.ts
+++ src/app/feature/components/main-canvas/form-field/form-field.ts
@@ -2,41 +2,34 @@ import { Component, computed, inject, input } from '@angular/core';
 import { FieldTypesService } from '../../../services/field-types.service';
 import { FormField } from '../../../models/field';
 import { NgComponentOutlet, TitleCasePipe } from '@angular/common';
-import { MatButtonModule} from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { FormService } from '../../../services/form.service';
+import { FieldPreview } from "../field-preview/field-preview";
 @Component({
   selector: 'app-form-field',
-  imports: [NgComponentOutlet,TitleCasePipe, MatButtonModule, MatIconModule],
+  imports: [NgComponentOutlet, TitleCasePipe, MatButtonModule, MatIconModule, FieldPreview],
   // templateUrl: './form-field.html',
   template: `
     <div class="content__form-field">
-    <div class="flex items-center justify-between mb-1">
-        <span>{{field().type | titlecase}}</span>
+      <div class="flex items-center justify-between mb-1">
+        <span>{{ field().type | titlecase }}</span>
         <button mat-icon-button (click)="deleteField($event)">
           <mat-icon class="-mr-2">delete</mat-icon>
         </button>
-    </div>
-    <ng-container [ngComponentOutlet]="previewComponent()"
-    [ngComponentOutletInputs]="{field: field()}"
-    ></ng-container>
+      </div>
+      <app-field-preview [field]="field()"></app-field-preview>
     </div>
   `,
-  styleUrl: './form-field.scss'
+  styleUrl: './form-field.scss',
 })
 export class FormFieldComponent {
-field = input.required<FormField>();
-formService = inject(FormService)
+  field = input.required<FormField>();
+  formService = inject(FormService);
+  fieldTypesService = inject(FieldTypesService);
 
-fieldTypesService = inject(FieldTypesService)
-
-previewComponent = computed(() =>{
-  const type = this.fieldTypesService.getFieldType(this.field().type);
-  return type?.component ?? null;
-})
-
-deleteField(e: Event){
-  e.stopPropagation()
-  this.formService.deleteField(this.field().id)
-}
+  deleteField(e: Event) {
+    e.stopPropagation();
+    this.formService.deleteField(this.field().id);
+  }
 }
